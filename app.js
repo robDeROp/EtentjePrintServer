3d021f937412715caf0e67843de5c3b9710f17a6
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-01-18 17:23:09 +0100

    FirstFullDeploy

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const { response } = require('express');
+
+const app = express();
+
+app.use(cors());
+
+mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'ID362979_Etentje.db.webhosting.be',
+  user: 'ID362979_Etentje',
+  password: 'Sp15021!',
+  database: 'ID362979_Etentje'
+})
+
+app.get('/BarOrder', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('SELECT O.Family_ID, O.Table_ID, P.Description, D.Quantity FROM Orders O JOIN OrderDetails D ON O.ID=D.OrderID JOIN Products P ON D.ProductID = P.ID WHERE P.Category = "Drank" AND O.ID IN ( SELECT MIN(ID) FROM Orders WHERE BarPrint="0" )',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
+app.get('/KeukenOrder', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('SELECT O.Family_ID, O.Table_ID, P.Description, D.Quantity FROM Orders O JOIN OrderDetails D ON O.ID=D.OrderID JOIN Products P ON D.ProductID = P.ID WHERE P.Category = "Voorgerecht" || P.Category = "Hoofdgerecht" || P.Category = "Dessert" AND O.ID IN ( SELECT MIN(ID) FROM Orders WHERE BarPrint="0" )',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
