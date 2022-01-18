c04af34473b346167bf5a72a204d85ae2761fcd5
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-01-18 17:28:26 +0100

    Update Fixed

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var connection = mysql.createConnection({
   database: 'ID362979_Etentje'
 })
 
+/*GET THE ORDER*/
+
 app.get('/BarOrder', function(req, res){ //GET method to access DB and return results in JSON
   connection.query('SELECT O.Family_ID, O.Table_ID, P.Description, D.Quantity FROM Orders O JOIN OrderDetails D ON O.ID=D.OrderID JOIN Products P ON D.ProductID = P.ID WHERE P.Category = "Drank" AND O.ID IN ( SELECT MIN(ID) FROM Orders WHERE BarPrint="0" )',
   function(err, rows, fields){
@@ -27,8 +29,37 @@ app.get('/BarOrder', function(req, res){ //GET method to access DB and return re
     res.end(JSON.stringify(data));
   });
 });
+
 app.get('/KeukenOrder', function(req, res){ //GET method to access DB and return results in JSON
-  connection.query('SELECT O.Family_ID, O.Table_ID, P.Description, D.Quantity FROM Orders O JOIN OrderDetails D ON O.ID=D.OrderID JOIN Products P ON D.ProductID = P.ID WHERE P.Category = "Voorgerecht" || P.Category = "Hoofdgerecht" || P.Category = "Dessert" AND O.ID IN ( SELECT MIN(ID) FROM Orders WHERE BarPrint="0" )',
+  connection.query('SELECT O.Family_ID, O.Table_ID, P.Description, D.Quantity FROM Orders O JOIN OrderDetails D ON O.ID=D.OrderID JOIN Products P ON D.ProductID = P.ID WHERE P.Category = "Voorgerecht" || P.Category = "Hoofdgerecht" || P.Category = "Dessert" AND O.ID IN ( SELECT MIN(ID) FROM Orders WHERE KeukenPrint="0" )',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
+
+/*Change ORDER PRINT STATE*/
+
+app.get('/BarOrderPrinted', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('UPDATE Orders SET BarPrint= "1" WHERE ID = ""',
+  function(err, rows, fields){
+    if(err) throw err;
+    var data = [];
+    for(i=0;i<rows.length;i++){
+      data.push(rows[i]);
+    }
+    console.log(JSON.stringify(data));
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.get('/KeukenOrderPrinted', function(req, res){ //GET method to access DB and return results in JSON
+  connection.query('UPDATE Orders SET KeukenPrint= "1" WHERE ID = ""',
   function(err, rows, fields){
     if(err) throw err;
     var data = [];
